c1ff705f8565f558b6cf3f8c7796279e95567898
abhishek.k@comviva.com <abhishek.k@comviva.com>
2023-01-03 14:52:23 +0530

    Adding device calculation part

---
+++ app/src/main/java/com/litmethod/android/DataProcessing/ProcessedData.kt
@@ -0,0 +1,32 @@
+package com.litmethod.android.DataProcessing
+
+object ProcessedData {
+
+    fun calculateAverageHeartRate(){
+
+    }
+
+    fun calculateMaximumWeight(){
+
+    }
+
+    fun findAverage( data:Int, count:Int){
+
+    }
+
+    fun findTimeUnderTension(){
+
+    }
+
+    fun findRepsCalculated(){
+
+    }
+
+    fun calculateResistance(){
+
+    }
+
+    fun calculateCaloriesBurnt(){
+
+    }
+}
